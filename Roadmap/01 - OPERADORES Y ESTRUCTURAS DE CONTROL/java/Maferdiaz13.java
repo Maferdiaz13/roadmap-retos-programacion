fd5938a80fb8a0f20aacc40db2c391fb19ed9e56
Maferdiaz13 <95454906+Maferdiaz13@users.noreply.github.com>
2025-07-21 17:25:56 -0400

    Create Maferdiaz13.java

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/Maferdiaz13.java
@@ -0,0 +1,30 @@
+// Sitio oficia: https://www.java.com/es/
+
+// Comentarios en una linea 
+/* Comentarios en varias lineas 
+  Comentarios en varias lineas 
+  Comentarios en varias lineas 
+  Comentarios en varias lineas */
+
+/** Comentarios para documentar en Javadoc  */
+
+//Variables en JAVA-¿Como crearlas? 
+
+int numero;     //Variables 
+float iva=0.16; //Constatntes 
+
+byte myByte = 100; // tipo de dato byte 
+short myShort = 1000;  // tipo de dato short 
+int myInt = 1; // tipo de dato entero 
+long myLong = 752398397;  // tipo de dato largo 
+float myFloat = 3.1416;  // tipo de dato flotante 
+double myDouble = 2.197347383;  // tipo de dato doble 
+chart myChart = 'M';  // tipo de dato chart NOTA: este tipo de datos solo permite colocar un solo carácter Unicode de 16 bits
+boolean myBoolean = 'true'; // tipo de dato booleano solo exite la opcion 'verdadero' o 'falso'
+
+//Sintaxis para imprimir por terminal en JAVA 
+System.out.print("Hola, JAVA!")
+
+
+
+
